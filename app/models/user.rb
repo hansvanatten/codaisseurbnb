4f16812079e639fa281388a9bd825d5d94157d5d
hansvanatten <hansvanatten@gmail.com>
2017-01-17 21:49:35 +0100

    One-to-One Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   has_many :rooms, dependent: :destroy
+
+  has_one :profile
+
+  def has_profile?
+        profile.present? && !profile.id.nil?
+  end
+
+  def full_name
+    profile.full_name
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
