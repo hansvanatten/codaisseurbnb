5da1882682a117f36df76f16f4651efef27663d9
hansvanatten <hansvanatten@gmail.com>
2017-01-20 10:49:34 +0100

    Through Association: Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :rooms, dependent: :destroy
 
   has_one :profile
+  has_many :bookings, dependent: :destroy
+  has_many :booked_rooms, through: :bookings, source: :rooms
 
   def has_profile?
         profile.present? && !profile.id.nil?
